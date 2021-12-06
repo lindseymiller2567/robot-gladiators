8bbd38ebddb39bb04384c580a6a3a9d2764faa7f
Lindsey Dubnicka <lindseymiller2567@gmail.com>
2021-12-06 17:46:29 -0600

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -72,10 +72,15 @@ var fight = function (enemyName) {
 
 //run fight fuction to start game
 for (var i = 0; i < enemyNames.length; i++) {
-    for (var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1));
         var pickedEnemyName = enemyNames[i];
         enemyHealth = 50;
+        // debugger
         fight(pickedEnemyName);
+    } else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
     }
     // call fight function with enemy-robot
     fight(enemyNames[i]);
