6117f5bfd86645b43328e6d569277964217517d1
Lindsey Dubnicka <lindseymiller2567@gmail.com>
2021-12-09 18:16:58 -0600

    fixed player name null

--- assets/js/game.js
+++ assets/js/game.js
@@ -170,9 +170,20 @@ var randomNumber = function(min, max) {
     return value;
 };
 
+var getPlayerName = function() {
+    var name = "";
+
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+
+    console.log("Your robot's name is " + name);
+    return name
+}
+
 // this is an object
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
